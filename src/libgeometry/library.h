e4422907003a079a33cc77790d9b59cf0601b07e
JuliaPech <DrakulinaJ@yandex.ru>
2021-04-30 21:23:43 +0700

    Update src/libgeometry/library.h

--- src/libgeometry/library.h
+++ src/libgeometry/library.h
@@ -1,6 +1,12 @@
 #pragma once
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int name(char* array);
 int check_in_parenthes(char* array);
 int check_space_1(char* array);
 int check_space_2(char* array);
+float area1(float radius);
+float perimetr1(float radius);

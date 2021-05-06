4de35a40bcedab44bc379c7b063ba104a396e998
JuliaPech <DrakulinaJ@yandex.ru>
2021-05-06 15:19:32 +0700

    Update src/libgeometry/library.h

--- src/libgeometry/library.h
+++ src/libgeometry/library.h
@@ -4,9 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct circle {
+    float x;
+    float y; 
+    float radius;
+};
+
 int name(char* array);
 int check_in_parenthes(char* array);
 int check_space_1(char* array);
 int check_space_2(char* array);
 float area1(float radius);
 float perimetr1(float radius);
+void intersects(int n, struct circle* array_circle);
